5a462962279a0ca76a3da995e789136282b8e9fc
Ahmad alam <ahmadalam15401.in>
2024-05-30 15:36:58 +0500

    take

--- cypress/example.js
+++
@@ -1,7 +0,0 @@
-describe('Demo store', () => {
-    it('Navbar', () => {
-  cy.visit("https://demo.nopcommerce.com/")
-  cy.xpath("//input[@class='search-box-text ui-autocomplete-input']").type('Apple MacBook Pro 13-inch')
-  cy.xpath("//button[@class='button-1 search-box-button']").click()
-    })
-})
